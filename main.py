30c5ade1cf1319667d6f826557a44ad32a7f0287
IwonaGorska <iwonagorskaa@gmail.com>
2020-12-05 23:43:08 +0100

    -

--- main.py
+++ main.py
@@ -49,7 +49,63 @@ def zad1():
     return dataset
 
 
-def zad2(numberOfClusters = 5):
+def kmeans(numberOfClusters, palette, markers):
+    X, y_true = make_blobs(n_samples=1000, centers=numberOfClusters, cluster_std=0.99, random_state=3042019)
+    df = pd.DataFrame(X, columns=['f1', 'f2'])
+    df.head()
+
+    print('Zbiór przed klasteryzacją: \n', df)
+
+    kmeans = KMeans(n_clusters=numberOfClusters)
+    kmeans.fit(df)
+    y_kmeans = kmeans.predict(df)
+
+    df['sklearn_cluster'] = y_kmeans
+    sklearn_centers = kmeans.cluster_centers_
+
+    sns.lmplot(data=df, x='f1', y='f2', fit_reg=False, hue='sklearn_cluster', markers=markers,
+               palette=palette).set(title='Wizualizacja grup - KMeans')
+    plt.scatter(sklearn_centers[:, 0], sklearn_centers[:, 1], c='black', s=100, alpha=0.5)
+    plt.show()
+
+    print('Zbiór po klasteryzacji (kolumna sklearn_cluster to przypisana etykieta klas): \n', df)
+
+
+def kmeans_pp(numberOfClusters, palette, markers):
+    X, y_true = make_blobs(n_samples=1000, centers=numberOfClusters, cluster_std=0.99, random_state=3042019)
+    df = pd.DataFrame(X, columns=['f1', 'f2'])
+    df.head()
+
+    kmeans = KMeans(n_clusters=numberOfClusters, init='k-means++', max_iter=300, n_init=10, random_state=0)
+    kmeans.fit(df)
+    y_kmeans = kmeans.predict(df)
+
+    df['sklearn_cluster'] = y_kmeans
+    sklearn_centers = kmeans.cluster_centers_
+
+    sns.lmplot(data=df, x='f1', y='f2', fit_reg=False, hue='sklearn_cluster', markers=markers,
+               palette=palette).set(title='Wizualizacja grup - KMeans++')
+    plt.scatter(sklearn_centers[:, 0], sklearn_centers[:, 1], c='black', s=100, alpha=0.5)
+    plt.show()
+
+
+def zad2(numberOfClusters=5):
+
+    wholePalette = ['#10fbbb', '#fbe610', '#eb6c6a', '#6aeb6c', '#6c6aeb', '#8b0000', '#002e53', '#fb109e']
+    allMarkers = ['o', '*', '+', '^', 'x', 's', 'v', 'w']
+    numberOfClusters = int(input("Podaj liczbe klastrow (max 8) lub wciśnij Enter, by przyjąć domyślną wartość (5): ") or 5)
+    if numberOfClusters > 8:
+        numberOfClusters = 8
+    print(numberOfClusters)
+    palette = wholePalette[0:numberOfClusters]
+    markers = allMarkers[0:numberOfClusters]
+    print(palette)
+    print(markers)
+
+    # df = zad1()  - in the future put this as a parameter into below functions as well
+    kmeans(numberOfClusters, palette, markers)
+    kmeans_pp(numberOfClusters, palette, markers)
+
     # I’ll use the StandardScaler class . This class implements a type of feature scaling
     # called standardization.Standardization scales, or shifts, the values for each numerical
     # feature in your dataset so that the features have a mean of 0 and standard deviation of 1
@@ -90,20 +146,8 @@ def zad2(numberOfClusters = 5):
     # plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=300, c='red')
     # plt.show()
 
-    X, y_true = make_blobs(n_samples=1000, centers=numberOfClusters, cluster_std=0.99, random_state=3042019)
-    df = pd.DataFrame(X, columns=['f1', 'f2'])
-    df.head()
 
-    kmeans = KMeans(n_clusters=numberOfClusters)
-    kmeans.fit(df)
-    y_kmeans = kmeans.predict(df)
 
-    df['sklearn_cluster'] = y_kmeans
-    sklearn_centers = kmeans.cluster_centers_
 
-    sns.lmplot(data=df, x='f1', y='f2', fit_reg=False, hue='sklearn_cluster', markers=['o', '*', '+', '^', 'x'],
-               palette=['#10fbbb', '#fbe610', '#eb6c6a', '#6aeb6c', '#6c6aeb']).set(title='Wizualizacja grup')
-    plt.scatter(sklearn_centers[:, 0], sklearn_centers[:, 1], c='black', s=100, alpha=0.5)
-    plt.show()
 
 zad2()
