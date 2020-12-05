59c25289d29fe8c56a2fe103767628af170d7c62
IwonaGorska <iwonagorskaa@gmail.com>
2020-12-05 18:55:40 +0100

    -

--- main.py
+++ main.py
@@ -5,13 +5,25 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from kneed import KneeLocator
 from sklearn.datasets import make_blobs
-from sklearn.cluster import KMeans
-from sklearn.metrics import silhouette_score
 from sklearn.preprocessing import StandardScaler
+
+import tarfile
+import urllib
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+from sklearn.metrics import silhouette_score, adjusted_rand_score
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 
 
 def zad1():
-    dataset = pd.read_csv('Sales_Transactions_Dataset_Weekly.csv')
+    dataset = pd.read_csv('Sales_Transactions_Dataset_Weekly.csv', header=True)
     pd.DataFrame = dataset
     valueCount = pd.DataFrame.size
     uniqueValueCount = pd.DataFrame.nunique()
@@ -42,32 +54,40 @@ def zad2(numberOfClusters = 5):
     # called standardization.Standardization scales, or shifts, the values for each numerical
     # feature in your dataset so that the features have a mean of 0 and standard deviation of 1
 
-    # dataset = zad1()
-    dataset, true_labels = make_blobs(
-        n_samples=200,
-        centers=3,
-        cluster_std=2.75,
-        random_state=42
-    )
+    df = zad1()
+    # DF <- data.frame
 
-    scaler = StandardScaler()
-    normalizedDataset = scaler.fit_transform(dataset)
-    kmeans = KMeans(
-        init="random",
-        n_clusters=numberOfClusters,
-        n_init=10,
-        max_iter=300,
-        random_state=42
-    )
-    kmeans.fit(dataset)
-    # The lowest SSE value
-    print('The lowest SSE value: ', kmeans.inertia_)
-    # Final locations of the centroid
-    print('Final locations of the centroid: \n', kmeans.cluster_centers_)
-    # The number of iterations required to converge
-    print('The number of iterations required to converge: ', kmeans.n_iter_)
-    # The cluster assignments are stored as a one-dimensional NumPy array
-    # in kmeans.labels_. Here’s a look at the first five predicted labels:
-    print('First five predicted labels of cluster assignments: ', kmeans.labels_[:5])
+    # dataset, true_labels = make_blobs(
+    #     n_samples=200,
+    #     centers=3,
+    #     cluster_std=2.75,
+    #     random_state=42
+    # )
+    #
+    # scaler = StandardScaler()
+    # normalizedDataset = scaler.fit_transform(dataset)
+    # kmeans = KMeans(
+    #     init="random",
+    #     n_clusters=numberOfClusters,
+    #     n_init=10,
+    #     max_iter=300,
+    #     random_state=42
+    # )
+    # kmeans.fit(dataset)
+    # # The lowest SSE value
+    # print('The lowest SSE value: ', kmeans.inertia_)
+    # # Final locations of the centroid
+    # print('Final locations of the centroid: \n', kmeans.cluster_centers_)
+    # # The number of iterations required to converge
+    # print('The number of iterations required to converge: ', kmeans.n_iter_)
+    # # The cluster assignments are stored as a one-dimensional NumPy array
+    # # in kmeans.labels_. Here’s a look at the first five predicted labels:
+    # print('First five predicted labels of cluster assignments: ', kmeans.labels_[:5])
+
+    # kmeans = KMeans(n_clusters=numberOfClusters, init='k-means++', max_iter=300, n_init=10, random_state=0)
+    # pred_y = kmeans.fit_predict(X)
+    # plt.scatter(X[:, 0], X[:, 1])
+    # plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=300, c='red')
+    # plt.show()
 
 zad2()
